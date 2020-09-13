21a3193625410a4d2018153be18a03ed011daf51
Rehan <softylogics@gmail.com>
2020-09-13 13:31:59 +0500

    w

--- Assets/TouchBehavior.cs
+++ Assets/TouchBehavior.cs
@@ -6,11 +6,12 @@ using UnityEngine.UI;
 public class TouchBehavior : MonoBehaviour
 {
 
-    public Text text;
+    public GameObject Car;
+    public SpriteRenderer sr;
     public Touch touch;
     private float timeTouchEnded;
     private float displayTime = .5f;
-    public Text text3;
+    
 
 
     // Update is called once per frame
@@ -19,7 +20,13 @@ public class TouchBehavior : MonoBehaviour
         if(Input.touchCount  > 0)
         {
             touch = Input.GetTouch(0);
-            text.text = touch.phase.ToString();
+
+            if (touch.phase == TouchPhase.Began)
+            {
+              
+
+            }
+            touch.phase.ToString();
             
             if(touch.phase == TouchPhase.Ended)
             {
@@ -29,7 +36,7 @@ public class TouchBehavior : MonoBehaviour
         }
         else if (Time.time - timeTouchEnded  > displayTime)
         {
-            text.text = "";
+            
         }
     }
 }
